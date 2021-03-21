5223374fdcaf990defd3b52b20b6c15795ba861f
Navven Raj MN <navvenr96@gmail.com>
2021-03-22 00:10:50 +0530

    Restaurant Controller feature update
    API Endpoint for getRestaurantByName Implemented

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -42,4 +42,18 @@ If error throws exception with error code and error message.
         List<RestaurantEntity> restaurantEntities = restaurantDao.restaurantsByRating();
         return restaurantEntities;
     }
+
+    /* This method is to get restaurants By Name and returns list of RestaurantEntity. its takes restaurant name as the input string.
+If error throws exception with error code and error message.
+*/
+    public List<RestaurantEntity> restaurantsByName(String restaurantName) throws RestaurantNotFoundException {
+        if (restaurantName == null || restaurantName == "") { //Checking for restaunrant name to be null or empty to throw exception.
+            throw new RestaurantNotFoundException("RNF-003", "Restaurant name field should not be empty");
+        }
+
+        //Calls restaurantsByName of restaurantDao to get list of RestaurantEntity
+        List<RestaurantEntity> restaurantEntities = restaurantDao.restaurantsByName(restaurantName);
+        return restaurantEntities;
+    }
+
 }
