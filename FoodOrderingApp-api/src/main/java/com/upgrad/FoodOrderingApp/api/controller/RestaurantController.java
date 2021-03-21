fba744a1f15f7bef484e5c4f3a9a78f1ea9c55a1
Navven Raj MN <navvenr96@gmail.com>
2021-03-22 00:10:50 +0530

    Restaurant Controller feature update
    API Endpoint for getRestaurantByName Implemented

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -8,14 +8,12 @@ import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
 import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.LinkedList;
@@ -93,5 +91,74 @@ public class RestaurantController {
         RestaurantListResponse restaurantListResponse = new RestaurantListResponse().restaurants(restaurantLists);
         return new ResponseEntity<RestaurantListResponse>(restaurantListResponse, HttpStatus.OK);
     }
+
+    /* The method handles get Restaurant By Name. It takes Restaurant name as the path variable.
+       & produces response in RestaurantListResponse and returns list of restaurant with details from the db. If error returns error code and error message.
+       */
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET, path = "/restaurant/name/{restaurant_name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<RestaurantListResponse> getRestaurantByName(@PathVariable(value = "restaurant_name") final String restaurantName) throws RestaurantNotFoundException {
+
+        //Calls restaurantsByName method of restaurantService to get the list of restaurant entity.
+        List<RestaurantEntity> restaurantEntities = restaurantService.restaurantsByName(restaurantName);
+
+        if (!restaurantEntities.isEmpty()) {//Checking if the restaurant entity is empty
+
+            //Creating restaurant list for the response
+            List<RestaurantList> restaurantLists = new LinkedList<>();
+            for (RestaurantEntity restaurantEntity : restaurantEntities) {  //Looping for each restaurant entity in restaurantEntities
+
+                //Calls  getCategoriesByRestaurant to get categories of the corresponding restaurant.
+                List<CategoryEntity> categoryEntities = categoryService.getCategoriesByRestaurant(restaurantEntity.getUuid());
+                String categories = new String();
+                ListIterator<CategoryEntity> listIterator = categoryEntities.listIterator();
+                //To concat the category names.
+                while (listIterator.hasNext()) {
+                    categories = categories + listIterator.next().getCategoryName();
+                    if (listIterator.hasNext()) {
+                        categories = categories + ", ";
+                    }
+                }
+
+                //Creating the RestaurantDetailsResponseAddressState for the RestaurantDetailsResponseAddress
+                RestaurantDetailsResponseAddressState restaurantDetailsResponseAddressState = new RestaurantDetailsResponseAddressState()
+                        .id(UUID.fromString(restaurantEntity.getAddressId().getState().getUuid()))
+                        .stateName(restaurantEntity.getAddressId().getState().getStateName());
+
+                //Creating the RestaurantDetailsResponseAddress for the RestaurantList
+                RestaurantDetailsResponseAddress restaurantDetailsResponseAddress = new RestaurantDetailsResponseAddress()
+                        .id(UUID.fromString(restaurantEntity.getAddressId().getUuid()))
+                        .city(restaurantEntity.getAddressId().getCity())
+                        .flatBuildingName(restaurantEntity.getAddressId().getFlatBuildingNumber())
+                        .locality(restaurantEntity.getAddressId().getLocality())
+                        .pincode(restaurantEntity.getAddressId().getPincode())
+                        .state(restaurantDetailsResponseAddressState);
+
+                //Creating RestaurantList to add to list of RestaurantList
+                RestaurantList restaurantList = new RestaurantList()
+                        .id(UUID.fromString(restaurantEntity.getUuid()))
+                        .restaurantName(restaurantEntity.getRestaurantName())
+                        .averagePrice(restaurantEntity.getAveragePriceForTwo())
+                        .categories(categories)
+                        .customerRating(BigDecimal.valueOf(restaurantEntity.getCustomerRating()))
+                        .numberCustomersRated(restaurantEntity.getNumberOfCustomersRated())
+                        .photoURL(restaurantEntity.getPhotoUrl())
+                        .address(restaurantDetailsResponseAddress);
+
+                //Adding it to the list
+                restaurantLists.add(restaurantList);
+
+            }
+
+            //Creating the RestaurantListResponse by adding the list of RestaurantList
+            RestaurantListResponse restaurantListResponse = new RestaurantListResponse().restaurants(restaurantLists);
+            return new ResponseEntity<RestaurantListResponse>(restaurantListResponse, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<RestaurantListResponse>(new RestaurantListResponse(), HttpStatus.OK);
+        }
+
+    }
+
+
 
 }
